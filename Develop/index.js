93f6132494964770320f98d003527f1d2428581e
Olive Provencio-Johnson <120212494+Olive-Provencio-Johnson@users.noreply.github.com>
2023-03-06 17:14:14 -0800

    added a list of questions to the index.js, listed from the professional README full stack blog

--- Develop/index.js
+++ Develop/index.js
@@ -3,7 +3,49 @@
 
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is your project title?", 
+        name: "title",
+    },
+    {
+        type: "input", 
+        message: "Please include a brief description of your project",
+        name: "description",
+
+    },
+    {
+        type: "input", 
+        message: "Please include more information about your project, such as the 'what, why, and how' of the project",
+        name: "about",
+
+    },
+    {
+        type: "input", 
+        message: "Please include any specific installation instructions for your project here",
+        name: "installation",
+
+    },
+    {
+        type: "input", 
+        message: "Provide instructions and examples for use",
+        name: "usage",
+
+    },
+    {
+        type: "input", 
+        message: "List your collaborators, if any, with links to their GitHub profiles.",
+        name: "credits",
+
+    },
+    {
+        type: "input", 
+        message: "The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/)",
+        name: "license",
+
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
