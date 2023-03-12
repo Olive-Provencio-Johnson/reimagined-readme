97345798faef4452c8f5f12feb9ef96467ff1f21
Olive Provencio-Johnson <120212494+Olive-Provencio-Johnson@users.noreply.github.com>
2023-03-11 23:28:44 -0800

    corrected and replaced value with license in the renderlincense badge function which returns the license badge and link as a concatenated string.

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,23 +1,31 @@
 // Function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  let LicenseBadge = "";
-  if (value === "Apache 2.0 License") {
-    return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
-  } else if (value === "GNU GPLv3") {
-    return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
-  } else if (value === "BSD 3-Clause License") {
-    return "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
-  } else if (value === "MIT") {
-    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
-  } else if (value === "Eclipse Public License 1.0") {
-    return "[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)";
+  let licenseBadge = "";
+  let licenseLink = "";
+  if (license === "Apache 2.0 License") {
+    licenseBadge = "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]";
+    licenseLink = "https://opensource.org/licenses/Apache-2.0";
+  } else if (license === "GNU GPLv3") {
+    licenseBadge = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]";
+    licenseLink = "https://www.gnu.org/licenses/gpl-3.0";
+  } else if (license === "BSD 3-Clause License") {
+    licenseBadge =  "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)]";
+    licenseLink = "https://opensource.org/licenses/BSD-3-Clause";
+  } else if (license === "MIT") {
+    licenseBadge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]";
+    licenseLink = "https://opensource.org/licenses/MIT";
+  } else if (license === "Eclipse Public License 1.0") {
+    licenseBadge = "[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)]";
+    licenseLink = "https://opensource.org/licenses/EPL-1.0";
   }
+  return (licenseBadge, licenseLink);
 }
 
 // Function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// function renderLicenseLink(license) {}
+// combining this in the function above 
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -48,6 +56,7 @@ function generateMarkdown(data) {
   ---
 ## License
   ${data.license}
+  ${renderLicenseBadge(data.license)}
 
 `;
 }
